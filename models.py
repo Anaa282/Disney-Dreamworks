7da19ae49d574d0c5de5d20519aec7199108e930
anamican <anamican@anamican.168.1.121>
2025-04-06 23:57:37 -0400

    primer commit

---
+++ models.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class Pelicula(BaseModel):
+    titulo: str = Field(...,min_length=1)
+    anio: int = Field(...,gt=1900, lt=2100)
+    estudio: str
+    genero: str
+    activa: bool=True
+
+class PeliculaconId(Pelicula):
+    id: int
+
+class PeliculaUpdate(BaseModel):
+    titulo: Optional[str]=None
+    anio: Optional[int]=None
+    estudio: Optional[str]=None
+    genero: Optional[str]=None
+    activa: Optional[bool]=None
+
+class Personaje(BaseModel):
+    nombre: str = Field(...,min_length=1)
