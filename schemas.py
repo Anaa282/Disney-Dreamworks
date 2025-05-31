62cad2c4224608e693486231431290fb8c5feb16
anamican <anamican@anamican.168.1.121>
2025-05-31 17:06:49 -0400

    coneccion entre personajes y peliculas3

--- schemas.py
+++ schemas.py
@@ -27,7 +27,7 @@ class PeliculaResponse(PeliculaBase):
 class PersonajeBase(BaseModel):
     nombre: str
     protagonista: bool
-    pelicula: str
+    pelicula_id: int
     activo: bool = True
 
 class PersonajeCreate(PersonajeBase):
@@ -39,7 +39,7 @@ class PersonajeOut(PersonajeBase):
 class PersonajeUpdate(BaseModel):
     nombre: str | None = None
     protagonista: bool | None = None
-    pelicula: str | None = None
+    pelicula_id: int | None = None
     activo: bool | None = None
 
     class Config:
