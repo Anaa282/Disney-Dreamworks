0b57fdce1725e54a076a72f8c1125b08c0c9d3ec
anamican <anamican@anamican.168.1.121>
2025-06-02 23:20:32 -0400

    html personajes intento

--- database.py
+++ database.py
@@ -1,16 +1,15 @@
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql+asyncpg://peliculas_db_t797_user:A23XVNpV3RsG6jkmv6viV9SvECa0Do7E@dpg-d0htcc3uibrs739ohak0-a/peliculas_db_t797"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
-
